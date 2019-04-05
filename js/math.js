af5eaeeb14a70354e2ff330bd4ebcebbdac0cba1
sfaverman <sofiasd@yahoo.com>
2019-04-04 20:17:29 -0700

    Completed Part 2 Math functions 1-5

--- js/math.js
+++ js/math.js
@@ -1,11 +1,26 @@
 /*eslint-env browser*/
 
 //STEP 1
+/*var number = window.prompt("Please enter a number");
+window.alert("Absolute value is " + Math.abs(number));*/
 
 //STEP 2
+/*var number = window.prompt("Please enter a floating decimal number");
+window.alert("The rounded value is " + Math.round(number));*/
 
 //STEP 3
+/*
+var number = window.prompt("Please enter a floating decimal number");
+window.alert("The rounded value is " + Math.floor(number));
+*/
 
 //STEP 4
+/*
+var number = window.prompt("Please enter a floating decimal number");
+window.alert("The rounded value is " + Math.ceil(number));
+*/
 
 //STEP 5
+var number = window.prompt("Please enter a number");
+window.alert("The square root of " + number + " is " + Math.sqrt(number));
+

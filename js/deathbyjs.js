a797bc5f174ba94d5ca85405e709aa3583423906
sfaverman <sofiasd@yahoo.com>
2019-04-11 16:01:33 -0700

    Completed Part 4 Step 1-2

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -2,7 +2,50 @@
 
 //STEP 1
 
+/*
+var letterString;
+
+function alphabetic(str) {
+	"use strict";
+	let i, letterArray =[];
+	for (i = 0; i < str.length; i++) {
+
+		letterArray.push(str.substr(i,1).toLowerCase());
+	}
+	  // console.log(letterArray);
+	return letterArray.sort().join("");
+}
+
+sortedString = alphabetic("Barbaristic");
+
+console.log(sortedString);
+
+console.log(alphabetic("Abracadabra"));
+
+*/
+
 //STEP 2
+
+var wordsString = 'the quick brown fox';
+
+function capWordsString(str) {
+	"use strict";
+	 let wordsArray = str.split(" ");
+	 console.log(wordsArray);
+
+	 for (let i = 0; i < wordsArray.length; i++) {
+		 wordsArray[i] = wordsArray[i].charAt(0).toUpperCase() + wordsArray[i].slice(1);
+		  // console.log("words array",wordsArray[i]);
+	 }
+
+	return wordsArray.join(" ");
+}
+
+updatedString = capWordsString(wordsString);
+
+console.log(updatedString);
+
+console.log(capWordsString("Software development services"));
 
 //STEP 3
 
